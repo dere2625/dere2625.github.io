8846d742be0edca963664214c6b13d854148b744
dere2625 <89865727+dere2625@users.noreply.github.com>
2022-08-02 02:51:23 -0500

    ajax call modified with callback methods

--- dictionary/dict.js
+++ dictionary/dict.js
@@ -9,53 +9,46 @@ jQuery(()=>{
         }
         removeChildren();
         removeElements();
-        var elements = [];
-        $.ajax({
-            url: "http://localhost:8081/words?"+word,
-            type: 'GET',
-            dataType: 'json', // added data type
-            success: function(res) {
-                res.forEach((item)=>{
-                    elements.push(item);
-                        // console.log(item);
-                        $(".suggestion").append(
-                            `<div class = "suggest">
-                            <h4>${item.word}</h4>
-                            <hr>
-                            </div>`
-                        )
-                    
-                })
-                let selected = '';
-                $(".suggest").click(function () {
-                    removeElements();
-                    selected = $(this).children("h4").text();
-                    $('.displayzone').css("display","flex");
-                    $('.suggestion').css("display","none");
-                    
-                    let response = $.ajax({
-                        url: "http://localhost:8081/words?"+word,
-                        type: 'GET',
-                        dataType: 'json', // added data type
-                        success: function(res) {
-                            for(response of res){
-                                $('.displayzone ol').append(
-                                    `<li>
-                                    <div class="content">
-                                        <h5>(${response.wordtype})</h5>
-                                        <q>${response.definition}</q>
-                                    </div>
-                                    </li>`
-                                )
-                            }
-                        }
-                    })
-                    
-                    
-                })
-            }
-        });
+        fetchAllData(word,(res)=>{
+            res.forEach((item)=>{
+                $(".suggestion").append(
+                    `<div class = "suggest">
+                    <h4>${item.word}</h4>
+                    <hr>
+                    </div>`
+                )
+            })
+
+            let selected = '';
+            $(".suggest").click(function () {
+                removeElements();
+                selected = $(this).children("h4").text();
+                $('.displayzone').css("display","flex");
+                $('.suggestion').css("display","none");
+                for(res of res){
+                    $('.displayzone ol').append(
+                        `<li>
+                        <div class="content">
+                            <h5>(${res.wordtype})</h5>
+                            <q>${res.definition}</q>
+                        </div>
+                        </li>`
+                    )
+                }
+            })
+        })
     })
+
+    function fetchAllData(query,showAll){
+        $.ajax({
+            url: "http://localhost:8081/words?"+query,
+            type: 'GET',
+            dataType: 'json',
+            success: function(res) {
+                showAll(res);
+            }
+    })}
+
 
     function removeChildren(){
         $(".suggest").remove();
